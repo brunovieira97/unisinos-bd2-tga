e1d7a289f46606d41b841e0c79b5e9f72b79bbcb
Bruno Vieira <bruno.vieira97@outlook.com>
2019-04-07 15:47:24 -0300

    Atualizada classe de Produto. Adicionada persistência com sequence, mapeadas colunas para os atributos e adicionado vínculo com Fornecedor.

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -5,34 +5,53 @@ import javax.persistence.*;
 @Entity
 public class Produto {
 
-	private int codigoProduto;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
+	@SequenceGenerator(name = "seq_produto", sequenceName = "s_produto", allocationSize = 1)
+	private int codigo;
+	
+	@Column(name = "descricao", nullable = false)
 	private String descricao;
-	private double preco;
 
-	public Produto(String descricao, double preco) {
+	@Column(name = "valor", nullable = false)
+	private double valor;
+
+	@ManyToOne(optional = true)
+	@JoinColumn(name = "codigofornecedor", foreignKey = @ForeignKey(name = "fk_produto_fornecedor"))
+	private Fornecedor fornecedor;
+
+	public Produto(String descricao, double valor) {
 		super();
 
 		this.descricao = descricao;
-		this.preco = preco;
+		this.valor = valor;
 	}
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
-	public void setPreco(double preco) {
-		this.preco = preco;
+	public void setValor(double valor) {
+		this.valor = valor;
 	}
 
-	public int getCodigoProduto() {
-		return this.codigoProduto;
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
+	public int getCodigo() {
+		return this.codigo;
 	}
 
 	public String getDescricao() {
 		return this.descricao;
 	}
 
-	public double getPreco() {
-		return this.preco;
+	public double getValor() {
+		return this.valor;
+	}
+
+	public Fornecedor getFornecedor() {
+		return this.fornecedor;
 	}
 }
